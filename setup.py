3d8278a55ae7341a899d9a22289125cd43cb48fd
Sébastien Petitdemange <sebastien.petitdemange@esrf.fr>
2014-12-16 18:15:36 +0100

    Build config tree + matias (dev/fixes)
    Add method get to create en instance's object by it's name
    
    added names_list property
    added references support: if name starts with '$' it is not indexed

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="beacon", version="0.1",
       description="BEAmline CONfiguration library",
       author="S. Petitdemange, M. Guijarro (ESRF)",
       package_dir={"beacon": "beacon"},
-      packages=["beacon", "beacon.conductor"]) 
+      packages=["beacon", "beacon.conductor","beacon.plugins"]) 
